b645f1457f98f915d28509dbb3f664daa584b678
Samuel Holland <samuel@sholland.org>
2017-02-21 19:00:42 -0600

    add TEMP_FAILURE_RETRY macro

--- include/sys/cdefs.h
+++ include/sys/cdefs.h
@@ -5,3 +5,14 @@
 #define __BEGIN_DECLS
 #define __END_DECLS
 #endif
+
+#ifndef TEMP_FAILURE_RETRY
+#define TEMP_FAILURE_RETRY(exp)                                                \
+  ({                                                                           \
+    typeof(exp) _rc;                                                           \
+    do {                                                                       \
+      _rc = (exp);                                                             \
+    } while (_rc == -1 && errno == EINTR);                                     \
+    _rc;                                                                       \
+  })
+#endif
